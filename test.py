d074c2cf3c9f65defe1c9bc50ffd151734601415
Fred Hsu <fredlhsu@aristanetworks.com>
2014-10-09 15:40:27 -0700

    reposition logos

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
+# lispview.py
+#
+# Author: Fred Hsu (fredlhsu@aristacom)
+#
+# Fetches data from map server and exports it to a file to be consumed
+# by d3.json
+
 import lispapi
 import json
+import time
 
 eidMap = {'10.1.1.0/24': 'eid-arista',
           '10.1.11.0/24': 'eid-arista',
@@ -9,24 +17,26 @@ rlocMap = {'1.1.1.1': 'arista',
            '1.1.11.1': 'arista',
            '4.4.4.4': 'hp'}
 
-lisp = lispapi.api_init("172.28.171.230", "root", "", 8080, api_debug=True)
-siteCache = lisp.get_site_cache()
-# print lisp.api_print()
-link = []
-eids = []
-rlocs = []
-siteNames = []
-for site in siteCache:
-    if site['registered'] == 'yes' and site['registered-rlocs']:
-        rlocs.append(site['registered-rlocs'][0]['address'])
-        siteNames.append(site['site-name'])
-        # print eidMap[site['eid-prefix']] \
-        #     + "-" + rlocMap[site['registered-rlocs'][0]['address']]
-        eid = eidMap[site['eid-prefix']]
-        rloc = rlocMap[site['registered-rlocs'][0]['address']]
-        link.append({'source': eid, 'target': rloc})
-print json.dumps({'link': link}, indent=2)
+while True:
+    lisp = lispapi.api_init("172.28.171.230", "root", "", 8080, api_debug=True)
+    siteCache = lisp.get_site_cache()
+    # print lisp.api_print()
+    link = []
+    eids = []
+    # rlocs = []
+    # siteNames = []
+    for site in siteCache:
+        if site['registered'] == 'yes' and site['registered-rlocs']:
+            eid = eidMap[site['eid-prefix']]
+            rloc = rlocMap[site['registered-rlocs'][0]['address']]
+            # rlocs.append(rloc)
+            # siteNames.append(site['site-name'])
+            link.append({'source': eid, 'target': rloc})
+    # write this to a file
+    print json.dumps({'link': link}, indent=2)
+    time.sleep(2)
 
+# server up web server
 
 # print "---EIDS---"
 # print eids
